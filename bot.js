242b722432b7396f723e9a2e2ae05de2679b2492
YaremaNatalia <yaremka.n@gmail.com>
2024-12-29 20:26:31 +0100

    а3

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const { TELEGRAM_TOKEN, RAILWAY_API_URL } = process.env;
 const token = "7643769299:AAF3mg373NtHhk3bUTFe608Hvm0QDmQQJDk";
-const url = " https://motivatebot-production.up.railway.app";
+const url = "https://motivatebot-production.up.railway.app";
 
 const bot = new TelegramBot(token, { polling: false });
 bot.setWebHook(`${url}/webhook`);
